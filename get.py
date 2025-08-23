f74c45b279cf05620b10af7172517760dd1195fb
Anjali0739 <anjalisoni0739@gmail.com>
2025-08-23 11:18:19 +0530

    added get.py

--- get.py
+++ get.py
@@ -1,5 +1,11 @@
 import requests
 import config
 
-r=requests.get(config.users())
-print(r.json())
+# r=requests.get(config.users())
+# print(r.json())
+
+url=config.users()
+print(url)
+r=requests.get(url)
+print(r.status_code)
+print(r.text)
