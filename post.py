61f245396c34f763d5e31d1ea6cf355e344c6e27
Anjali0739 <anjalisoni0739@gmail.com>
2025-08-23 12:20:10 +0530

    added post.py

--- post.py
+++ post.py
@@ -1,3 +1,20 @@
 import requests
 import config
 
+url = config.users()
+
+name=input("Enter your name: ")
+email=input("Enter your email: ")
+gender=input("Enter your gender: ")
+status=input("Enter the status: ")
+
+data=dict()
+data['name']=name
+data['email']=email
+data['gender']=gender
+data['status']=status
+
+# print(data)
+
+r=requests.post(url,data=data)
+print(r)
